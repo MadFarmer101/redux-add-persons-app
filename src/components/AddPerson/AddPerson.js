5ffec0467ccb757ddee3c0652c790dd0cc129557
Janko <mailodjanka@gmail.com>
2020-09-14 14:22:58 +0200

    Adds handlers for user input of name and age and passes them as arguments by clicking a add-person button

--- src/components/AddPerson/AddPerson.js
+++ src/components/AddPerson/AddPerson.js
@@ -7,13 +7,31 @@ class AddPerson extends Component {
     name: "",
     age: "",
   };
-  
+
+  changeNameHandler = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  changeAgeHandler = (event) => {
+    this.setState({ age: event.target.value });
+  };
+
   render() {
     return (
       <div className="AddPerson">
-        <input placeholder="Enter your name"></input>
-        <input placeholder="Enter your age"></input>
-        <button onClick={this.props.personAdded}>Add Person</button>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={this.changeNameHandler}
+          placeholder="Enter your name"
+        ></input>
+        <input
+          type="number"
+          value={this.state.age}
+          onChange={this.changeAgeHandler}
+          placeholder="Enter your age"
+        ></input>
+        <button onClick={() => this.props.personAdded(this.state.name, this.state.age)}>Add Person</button>
       </div>
     );
   }
